70bd356947e13e9f84284348dcdc9e01f09a0f8b
maxthetomas <max@maxthetomas.ru>
2025-04-23 06:20:06 +0400

    bump client to 0.2.1 (9)

--- src/main/java/ru/maxthetomas/craftminedailies/CraftmineDailies.java
+++ src/main/java/ru/maxthetomas/craftminedailies/CraftmineDailies.java
@@ -50,7 +50,7 @@ public class CraftmineDailies implements ModInitializer {
     public static final String MOD_ID = "craftminedailies";
     public static final String WORLD_NAME = "_cmd_daily";
 
-    public static final int VERSION = 8;
+    public static final int VERSION = 9;
     private static String VERSION_STRING = String.valueOf(VERSION);
     public static boolean HAS_UPDATES = false;
 
