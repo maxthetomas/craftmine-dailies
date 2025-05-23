624091c1963ebd15c9be5a996a2df4739a6928ec
maxthetomas <max@maxthetomas.ru>
2025-05-23 22:46:56 +0300

    added random tick speed daily effect

--- src/main/java/ru/maxthetomas/craftminedailies/content/DailyWorldEffects.java
+++ src/main/java/ru/maxthetomas/craftminedailies/content/DailyWorldEffects.java
@@ -7,6 +7,7 @@ import net.minecraft.network.chat.Style;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.mines.WorldEffect;
 import ru.maxthetomas.craftminedailies.CraftmineDailies;
 import ru.maxthetomas.craftminedailies.mixin.common.WorldEffectBuilderInvoker;
@@ -15,28 +16,36 @@ public class DailyWorldEffects {
     public static Style DAILY_WORLD_EFFECT_STYLE = Style.EMPTY.withColor(ChatFormatting.GOLD);
 
     // Removes the death penalty
-    public static WorldEffect NO_DEATH_PENALTY = register(WorldEffect.builder("no_death_penalty")
+    public static final WorldEffect NO_DEATH_PENALTY = register(WorldEffect.builder("no_death_penalty")
             .withItemModelOf(Items.GRAVE_ADVANCEMENT)
             .withNameStyle(DAILY_WORLD_EFFECT_STYLE));
 
     // 2x on win, 0x on death
-    public static WorldEffect ALL_IN = register(WorldEffect.builder("all_in")
+    public static final WorldEffect ALL_IN = register(WorldEffect.builder("all_in")
             .withItemModelOf(Items.DIAMOND).xpModifier(2f)
             .withNameStyle(DAILY_WORLD_EFFECT_STYLE).incompatibleWith(NO_DEATH_PENALTY));
 
     // Time passes quicker when you don't pick up XP orbs,
     // Player's XP counts towards final score
-    public static WorldEffect XP_ADDICTION = register(WorldEffect.builder("xp_addiction")
+    public static final WorldEffect XP_ADDICTION = register(WorldEffect.builder("xp_addiction")
             .withItemModelOf(Items.EXPERIENCE_BOTTLE).incompatibleWith(NO_DEATH_PENALTY)
             .withNameStyle(DAILY_WORLD_EFFECT_STYLE));
 
     // You can die. Once.
-    public static WorldEffect STRONG_SPIRIT = register(WorldEffect.builder("strong_spirit")
+    public static final WorldEffect STRONG_SPIRIT = register(WorldEffect.builder("strong_spirit")
             .withItemModelOf(Items.TOTEM_OF_UNDYING)
             .withNameStyle(DAILY_WORLD_EFFECT_STYLE));
 
-    public static WorldEffect USED_STRONG_SPIRIT = register(WorldEffect.builder("used_strong_spirit")
+    public static final WorldEffect USED_STRONG_SPIRIT = register(WorldEffect.builder("used_strong_spirit")
             .withItemModelOf(Items.GRAVE_ADVANCEMENT)
+            .withNameStyle(DAILY_WORLD_EFFECT_STYLE).neverUnlocked().notRandomizable());
+
+    public static final WorldEffect RANDOM_TICK_SPEED = register(WorldEffect.builder("random_tick_speed")
+            .withItemModelOf(Items.CLOCK)
+            .onMineEnter(e -> e.getGameRules()
+                    .getRule(GameRules.RULE_RANDOMTICKING).set(30, e.theGame()))
+            .onMineLeave(e -> e.getGameRules()
+                    .getRule(GameRules.RULE_RANDOMTICKING).set(3, e.theGame()))
             .withNameStyle(DAILY_WORLD_EFFECT_STYLE).neverUnlocked().notRandomizable());
 
     private static WorldEffect register(WorldEffect.Builder builder, String itemModel) {
